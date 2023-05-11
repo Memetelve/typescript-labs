9f4dbab82f167b18f8e73584e65cd1693bc55a3d
Memetelve <maciek@marszalkowski.pl>
2023-05-11 18:36:47 +0200

    lab03

--- api/src/index.js
+++ api/src/index.js
@@ -7,21 +7,20 @@ app.use(cors());
 app.use(json());
 
 const users = [
-    { id: 1, name: 'Miguel', age: 22 },
-    { id: 2, name: 'Juan', age: 23 },
-    { id: 3, name: 'Pedro', age: 24 },
-    { id: 4, name: 'Luis', age: 25 },
-    { id: 5, name: 'Carlos', age: 26 },
-    { id: 6, name: 'Andres', age: 27 },
-    { id: 7, name: 'Jorge', age: 28 },
-    { id: 8, name: 'Jose', age: 29 },
-    { id: 9, name: 'Ricardo', age: 30 },
-    { id: 10, name: 'Miguel', age: 31 },
-    { id: 11, name: 'Juan', age: 32 },
-    { id: 12, name: 'Pedro', age: 33 },
-    { id: 13, name: 'Luis', age: 34 },
-    { id: 14, name: 'Carlos', age: 26 },
-
+    { id: 1, name: 'Miguel', age: 22, status: "active" },
+    { id: 2, name: 'Juan', age: 23, status: "active" },
+    { id: 3, name: 'Pedro', age: 24, status: "active" },
+    { id: 4, name: 'Luis', age: 25, status: "active" },
+    { id: 5, name: 'Carlos', age: 26, status: "active" },
+    { id: 6, name: 'Andres', age: 27, status: "active" },
+    { id: 7, name: 'Jorge', age: 28, status: "active" },
+    { id: 8, name: 'Jose', age: 29, status: "active" },
+    { id: 9, name: 'Ricardo', age: 30, status: "active" },
+    { id: 10, name: 'Miguel', age: 31, status: "active" },
+    { id: 11, name: 'Juan', age: 32, status: "active" },
+    { id: 12, name: 'Pedro', age: 33, status: "active" },
+    { id: 13, name: 'Luis', age: 34, status: "active" },
+    { id: 14, name: 'Carlos', age: 26, status: "active" }
 ]
 
 
@@ -37,15 +36,22 @@ app.get('/users', (req, res) => {
 app.get('/user/:id', (req, res) => {
     const { id } = req.params;
     const user = users.find(user => user.id == id);
+
+    if (!user) {
+        res.status(404).json({ error: 'User not found' });
+    }
+
     res.json(user);
 });
 
 app.get('/username/:name/:limit?', (req, res) => {
     let { name, limit } = req.params;
 
-    if (!limit) {
-        limit = 1;
+    if (!user) {
+        res.status(404).json({ error: 'User not found' });
     }
+
+    limit = limit || 1;
 
     const user = users.filter(user => user.name == name).splice(0, limit);
     res.json(user);
@@ -61,8 +67,18 @@ app.post('/user', (req, res) => {
 
 app.put('/user/:id', (req, res) => {
     const { id } = req.params;
+
+    if (Object.keys(req.body).every(x => x in ["name", "age"])) {
+        res.status(400).json({ error: 'Unexpected params' });
+    }
+
     const { name, age } = req.body;
+
     const index = users.findIndex(user => user.id == id);
+    if (!user) {
+        res.status(404).json({ error: 'User not found' });
+    }
+
     const user = { id, name, age };
     users[index] = user;
     res.json(user);
@@ -71,10 +87,34 @@ app.put('/user/:id', (req, res) => {
 app.delete('/user/:id', (req, res) => {
     const { id } = req.params;
     const index = users.findIndex(user => user.id == id);
+
+    if (index < 0) {
+        res.status(404).json({ error: 'User not found' });
+    }
+
     const user = users.splice(index, 1);
     res.json(user);
 });
 
+app.patch("user/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, age } = req.body;
+    const index = users.findIndex(user => user.id == id);
+
+    if (Object.keys(req.body).every(x => x in ["name", "age"])) {
+        res.status(400).json({ error: 'Unexpected params' });
+    }
+
+    if (index < 0) {
+        res.status(404).json({ error: 'User not found' });
+    }
+
+    const user = users[index];
+    user.name = name || user.name;
+    user.age = age || user.age;
+
+    res.json(user);
+})
 
 app.listen(port, () => {
     console.log(`Api listening on port ${port}`);
